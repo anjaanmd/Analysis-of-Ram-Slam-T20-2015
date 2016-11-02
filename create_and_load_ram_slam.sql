8d4040411436a53c4bbb844d6af6a3721b2f3a88
anjaanmd <mohandoss.ez@gmail.com>
2016-11-03 04:56:20 +0530

    added 'optionally enclosed by quote' in load data infile procedure

--- create_and_load_ram_slam.sql
+++ create_and_load_ram_slam.sql
@@ -70,12 +70,12 @@ CREATE TABLE ram_slam_bowling(
     )DEFAULT CHARSET=utf8;
 
 LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.match_details.csv' INTO TABLE ram_slam_match_details 
- FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+ FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.batting.csv' INTO TABLE  ram_slam_batting FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
-
-
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.bowling.csv' INTO TABLE  ram_slam_bowling FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.batting.csv' INTO TABLE  ram_slam_batting FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.score_details.csv' INTO TABLE  ram_slam_score_details FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.bowling.csv' INTO TABLE  ram_slam_bowling FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
+
+
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.score_details.csv' INTO TABLE  ram_slam_score_details FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
--- loading_data_ram_slam.sql
+++ loading_data_ram_slam.sql
@@ -2,12 +2,12 @@ USE ram_slam_2015;
 
 
 LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.match_details.csv' INTO TABLE ram_slam_match_details 
- FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+ FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.batting.csv' INTO TABLE  ram_slam_batting FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
-
-
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.bowling.csv' INTO TABLE  ram_slam_bowling FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.batting.csv' INTO TABLE  ram_slam_batting FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
 
-LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.score_details.csv' INTO TABLE  ram_slam_score_details FIELDS TERMINATED BY ','   LINES TERMINATED BY '\n' IGNORE 1 LINES;
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.bowling.csv' INTO TABLE  ram_slam_bowling FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
+
+
+LOAD DATA INFILE '/var/lib/mysql-files/2015_south_ram_slam_t20.score_details.csv' INTO TABLE  ram_slam_score_details FIELDS TERMINATED  BY ',' OPTIONALLY ENCLOSED BY '"'    LINES TERMINATED BY '\n' IGNORE 1 LINES;
